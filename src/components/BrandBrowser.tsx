dc8fe4956c8ebbd7e618760a106c1049a34a8ec9
obregona <obregonariel@gmail.com>
2026-03-07 12:04:31 -0300

    Update BrandBrowser.tsx

--- src/components/BrandBrowser.tsx
+++ src/components/BrandBrowser.tsx
@@ -64,7 +64,7 @@ const BrandBrowser = () => {
                       </div>
                     </div>
                     <span className="text-[10px] text-muted-foreground">
-                      {s.pegLabel} - {s.ballEndLabel}
+                      {s.pegLabel}    -    {s.ballEndLabel}
                     </span>
                   </div>
                 ))}
